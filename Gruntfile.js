596616931bf30dc98c9fc40da7583e53b3e80b01
bmsrox <bmsrox@gmail.com>
2016-07-11 18:15:45 -0300

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+
+    'electron-packager': {
+        'build-linux': {
+          options:{
+            platform  : 'linux',
+            arch      : 'x64',
+            dir       : '.',
+            out       : './build',
+            icon      : '',
+            name      : 'app',
+            ignore    : 'node_modules',
+            version   : '1.2.6', // set version of electron
+            overwrite : true
+          }
+        },
+        'build-win': {
+          options:{
+            platform  : 'win32',
+            arch      : 'x64',
+            dir       : '.',
+            out       : './build',
+            icon      : '',
+            name      : 'app',
+            ignore    : 'node_modules',
+            version   : '1.2.6', // set version of electron
+            overwrite : true
+          }
+        },
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-electron-packager');
+  // Tarefas que serão executadas
+  grunt.registerTask( 'default', ['electron-packager:build-linux'] );
+
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+Electron Seed
+=============
+
+###Dependencies
+
+Install
+
+	npm install -g grunt
+	npm install -g bower
+  npm install -g electron-prebuilt
+
+run into project folder
+
+	npm install
+
+after
+
+	grunt
